ea8176a81911125e3fdcd13ae902fd245c32b1b9
iLOSTinYOU <87181511+orekixadi@users.noreply.github.com>
2022-12-18 22:35:59 +0530

    Update Theme.py

--- Yukki/Plugins/Theme.py
+++ Yukki/Plugins/Theme.py
@@ -2,7 +2,7 @@ from typing import Dict, List, Union
 
 from pyrogram import Client, filters
 
-from Yukki import BOT_USERNAME, MUSIC_BOT_NAME, app, db
+from Yukki import MUSIC_BOT_NAME, app, db
 from Yukki.Database import _get_theme, get_theme, save_theme
 from Yukki.Decorators.permission import PermissionCheck
 
